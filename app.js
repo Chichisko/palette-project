c4aa725d7b0c8396d372c49832aa42e139e695ac
Chichisko <61492597+Chichisko@users.noreply.github.com>
2020-02-27 14:23:26 +0500

    Site prototype created

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const path = require('path');
+const serveStatic = require('serve-static');
 
 app.engine('ejs', require('ejs-locals'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(serveStatic(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.render('main');
